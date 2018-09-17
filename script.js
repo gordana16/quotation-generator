5a134b9a9e38953d8fb8d12e0ea3c753fe61d9ca
Gordana Nikolic <gordana.nikolic6@gmail.com>
2018-09-17 22:45:07 +0100

    adding some css

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var beginning = ['Strive for ', 'Don\'t let ', 'Don\'t stop ', 'Don\'t wait for ', 'Be the girl ', 'Don\'t give up; '];
-var middle = ['progress ', 'small minds ', 'until ', 'opportunity ', 'who decided ', 'great things '];
-var end = ['not perfection', 'too big', 'you are proud', 'create it', 'to go for it', 'take time'];
+//define quote segments
+var beginning = ['Patience, persistence and perspiration ', 'Communication ', 'The secret of your success ', 'More than anything else, preparation ', 'Setting goals ', 'Before everything else, getting ready '];
+var middle = ['make an unbeatable combination ', 'is the key ', 'is determined ', 'is the key ', 'is the first step ', 'is the secret '];
+var end = ['for success', 'to personal and career success', 'by your daily agenda', 'to success', 'in turning the invisible into the visible', 'of success'];
 var punct = ['.', '!', '!!!'];
-
-document.querySelector(".quote").style.display = "none";
-document.querySelector('.btn-gen').addEventListener('click', function () {
-  var quote = generateRandomQuote();
-  document.querySelector(".quote").textContent = quote;
-  document.querySelector(".quote").style.display = "block";
-});
 
 function generateRandomQuote() {
   var quote = beginning[Math.floor(Math.random() * beginning.length)] + middle[Math.floor(Math.random() * middle.length)]
     + end[Math.floor(Math.random() * end.length)] + punct[Math.floor(Math.random() * punct.length)];
   return quote;
 }
+
+document.querySelector('.btn-gen').addEventListener('click', function () {
+  var quote = generateRandomQuote();
+  document.querySelector(".quote").innerHTML = '<p>' + quote + '</p>';
+});
+
